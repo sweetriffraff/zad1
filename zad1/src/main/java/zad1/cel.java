b46845e9ff1a201cba6f32500e32fe674d46975d
gszmigulan <44379945+gszmigulan@users.noreply.github.com>
2018-10-24 14:02:15 -0700

    Update cel.java

--- zad1/src/main/java/zad1/cel.java
+++ zad1/src/main/java/zad1/cel.java
@@ -6,9 +6,9 @@ public class cel {
 		{
 			double cel = Double.parseDouble(args[0]);
 			System.out.println(cel + "°C");
-			Fahrenheit fa= new Fahrenheit();
+			Fahrenheit faren= new Fahrenheit();
 			Kelvin k= new Kelvin();
-			double fah =fa.translate(cel);
+			double fah =faren.translate(cel);
 			double kel=k.translate(cel);
 			System.out.println(fah + "°F");
 			System.out.println(kel + " K");
